850121a893d7b05aeb422bf3843e189386a9c043
Neha-Raliya <70155047+Neha-Raliya@users.noreply.github.com>
2021-05-02 11:54:11 +0530

    initial files

---
+++ js/mentor.js
@@ -0,0 +1,12 @@
+function search1(){
+  document.querySelector("#r2").style.display = "none";
+document.querySelector("#r1").style.display = "inline";
+window.location.href = "#r1";
+}
+
+function search2(){
+
+document.querySelector("#r1").style.display = "none";
+  document.querySelector("#r2").style.display = "inline";
+  window.location.href = "#r2";
+}
